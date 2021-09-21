e6dbc4385be3a9cc1e1b68f9dc0f2920891a0e94
ghofloyaragh <ghofloyaragh@gmail.com>
2021-09-21 23:30:22 +0430

    add yop text

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,4 +15,17 @@ function serveLocalData() {
     });
 }
 
+var i = 0;
+var txt = 'گالری قفل و یراق کرمان'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("animated-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter()
 serveLocalData()
