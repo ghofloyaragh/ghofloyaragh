59aa3e0afbd573f94278a52a06d8a9432303ffe8
ghofloyaragh <ghofloyaragh@gmail.com>
2021-09-23 20:34:00 +0330

    add more text to top

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,18 +14,18 @@ function serveLocalData() {
         document.getElementById("photo-grid").appendChild(figure)
     });
 }
-
-var i = 0;
-var txt = 'گالری قفل و یراق کرمان'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("animated-text").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
-
-typeWriter()
+//
+// var i = 0;
+// var txt = ''; /* The text */
+// var speed = 50; /* The speed/duration of the effect in milliseconds */
+//
+// function typeWriter() {
+//     if (i < txt.length) {
+//         document.getElementById("animated-text").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+//
+// typeWriter()
 serveLocalData()
